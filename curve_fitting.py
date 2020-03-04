3ce75a925ed5c7acd5a9ca1c2b1a00d498ad1cc2
AlbusPeter <albuspeter.rzh@gmail.com>
2020-03-04 14:50:57 -0800

    first commit

---
+++ curve_fitting.py
@@ -0,0 +1,33 @@
+from scipy.optimize import curve_fit
+import numpy as np
+from nonlinear_functions import vonmises
+import matplotlib.pyplot as plt
+
+def curve_Fitting(func, x, y, initials, bounds):
+    ## make sure initials and bounds are coherent with func parameters ##
+    best_vals, covar = curve_fit(func, x, y, p0=initials, bounds=bounds)
+    return best_vals
+
+if __name__ == "__main__":
+    x = np.linspace(-np.pi, np.pi, 1000)
+
+    ## generate random von Mises samples ##
+    mu = 0
+    kappa = 8.0
+    amplitude = 10
+    y = vonmises(x, kappa, mu, amplitude)
+    noise = 1.0 * np.random.normal(size=y.size)
+    y += noise
+
+    ## Show original samples
+    plt.figure()
+    plt.scatter(x, y)
+
+    ## Curve fitting setups
+    initial_guess = [4,2,5]
+    params_bounds = (0, [20., 5., 20.])
+    params = curve_Fitting(vonmises, x, y, initial_guess, params_bounds)
+
+    ## Show fitted curve ##
+    plt.plot(x, vonmises(x, params[0], params[1], params[2]), 'r-')
+    plt.show()
---
+++ nonlinear_functions.py
@@ -0,0 +1,23 @@
+import numpy as np
+from numpy import exp, cos, sin, pi
+from scipy.special import i0
+
+## Exponential ##
+def exponential(x, a, b, c):
+    return a * exp(-b * x) + c
+
+## Gaussian ##
+def Gaussian(x, mu, sigma2, amplitude):
+    return amplitude * exp(-(x-mu)**2 / sigma2)
+
+## Derivative of Gaussian ##
+def DoG(x, mu, sigma2, amplitude):
+    return - amplitude * x * exp(-(x-mu)**2 / sigma2)
+
+## von Mises ##
+def vonmises(x, kappa, mu, amplitude):
+    return amplitude / (i0(kappa) * 2 * pi) * exp(kappa * cos(x - mu))
+
+## Derivative of von Mises ##
+def DoV(x, kappa, mu, amplitude):
+    return - amplitude / (i0(kappa) * 2 * pi) * exp(kappa * cos(x - mu)) * kappa * sin(x - mu)
